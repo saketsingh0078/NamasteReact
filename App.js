be09e2dc409e7b961668e0201a06ecd019e8f981
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-05-30 14:37:03 +0530

    eposide 3

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const header = React.createElement("h1", { id: "heading" }, "Hello from react");
+
+// React component
+const Title = () => {
+  return <h1>Hi from title </h1>;
+};
+
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>hi namaste react Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(<HeadingComponent />);
